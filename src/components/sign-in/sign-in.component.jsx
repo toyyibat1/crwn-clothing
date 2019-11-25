4bc1f724cebc9dc4fa96c2ce292124e0750d9e30
unknown <toyyibat.abdulkareem@gmail.com>
2019-11-25 10:03:20 +0100

    implemented firbase

---
+++ src/components/sign-in/sign-in.component.jsx
@@ -0,0 +1,64 @@
+import React from 'react';
+
+import FormInput from '../form-input/form-input.component';
+import CustomButton from '../custom-button/custom-button.component'
+import {signInWithGoogle} from '../../firebase/firebase.utils';
+import './sign-in.styles.scss';
+
+class SignIn extends React.Component{
+    constructor(){
+        super();
+
+        this.state = {
+            email: '',
+            password: ''
+        }
+    }
+    handleSubmit = e => {
+        e.preventDefault();
+
+        this.setState({email: '', password:''})
+    }
+
+    handleChange = event => {
+        const {value, name} = event.target;
+        this.setState({[value]: name})
+
+    }
+    render(){
+        return (
+            <div className="sign-in">
+                <h2>I already have an account</h2>
+                <span>Sign in with email and password</span>
+
+                <form onSubmit={this.handleSubmit}>
+                    <FormInput 
+                        name='email' 
+                        type='email' 
+                        handleChange={this.handleChange}
+                        value={this.state.email} 
+                        label='email'
+                        required
+                    />
+                    <FormInput
+                        name='password' 
+                        type='password' 
+                        handleChange={this.handleChange}
+                        value={this.state.password}
+                        label='password'
+                        required
+                    />
+                    <div className="buttons">
+                    <CustomButton type='submit'>Sign in</CustomButton>
+                    <CustomButton onClick={signInWithGoogle} isGoogleSignin>
+                        Sign in with Google
+                    </CustomButton>
+                    </div>
+                    
+                </form>
+            </div>
+        )
+    }
+}
+
+export default SignIn;
---
+++ src/firebase/firebase.utils.js
@@ -0,0 +1,54 @@
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+import 'firebase/auth';
+
+const config = {
+  apiKey: "AIzaSyBTvLE16taEeQ7OLSJ7jzJO1rYpIsEbr6M",
+  authDomain: "crwn-db4.firebaseapp.com",
+  databaseURL: "https://crwn-db4.firebaseio.com",
+  projectId: "crwn-db4",
+  storageBucket: "crwn-db4.appspot.com",
+  messagingSenderId: "1231452801",
+  appId: "1:1231452801:web:979989fc0487a2fdb86b80",
+  measurementId: "G-96RSPP17ZP"
+};
+
+export const createUserProfileDocument = async (userAuth, additionalData) => {
+  if (!userAuth) return;
+
+  const userRef = firestore.doc(`users/${userAuth.uid}`)
+
+  const snapShot = await userRef.get();
+
+  console.log(snapShot);
+  if(!snapShot.exists){
+    const {displayName, email} = userAuth;
+    const createdAt = new Date();
+
+    try{
+      await userRef.set({
+        displayName,
+        email,
+        createdAt,
+        ...additionalData
+      })
+
+    }
+    catch(error){
+      console.log('error creating users', error.message);
+    }
+  }
+  return userRef;
+};
+
+firebase.initializeApp(config);
+
+export const auth = firebase.auth();
+export const firestore = firebase.firestore();
+
+const provider = new firebase.auth.GoogleAuthProvider();
+provider.setCustomParameters({prompt: 'select_account'});
+export const signInWithGoogle = () => auth.signInWithPopup(provider);
+
+export default firebase;
+
